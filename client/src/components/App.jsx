159d8af0951bda2182a0187a882d075e7d8141f6
HanadiSabarnah <hsabarnah@gmail.com>
2020-11-04 18:35:32 +0200

    ohhhh

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+        return (<div>
+          <h4>Add more repos!</h4>
+         
+        </div>)
+      }
+    
+}
+
+
+export default App;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const db = require('./public/database');
+const db = require('./database');
 const app = express();
 
 app.use(express.static('public'));
